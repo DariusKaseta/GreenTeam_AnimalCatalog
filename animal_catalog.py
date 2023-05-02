4ad61536a0462b5b921bd4beffa853afa3d2b340
Milda Auglyte <auglyte.m@gmail.com>
2023-05-02 15:59:29 +0300

    pradzia

--- animal_catalog.py
+++ animal_catalog.py
@@ -1,1 +1,47 @@
+import os
 
+os.system('cls' if os.name == 'nt' else 'clear')
+
+class Animal:
+    def __init__(self, animal_class, order, family, genus, species):
+        self.animal_class = animal_class
+        self.order = order
+        self.family = family
+        self.genus = genus
+        self.species = species
+
+    def print_animal_info():
+        pass
+
+class Catalog: 
+    animals = []
+
+    def add_animal(): # Karolis
+        pass
+
+    def remove_animal(): # Karolis
+        pass
+
+    def review_catalog(): # Milda
+        pass
+
+    def review_by_class(): # Milda
+        pass
+        
+    def review_by_order(): # Milda
+        pass
+
+    def review_by_family(): # Milda
+        pass
+
+    def review_by_genus(): # Milda
+        pass
+
+    def review_by_species(): # Milda
+        pass
+
+    def total_animals_added(): #Darius
+        pass
+
+
+# Petras - save in json
